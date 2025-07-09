8a89524c589841feffe70fce32ff8546ff9583e3
@jainil@ <jainilthummar2005@gmail.com>
2025-07-09 16:42:09 +0530

    add coras

--- Program.cs
+++ Program.cs
@@ -12,7 +12,19 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HospitalHubContext>
     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 
+
 var app = builder.Build();
+// .NET 6 and later (Program.cs)
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowReactApp",
+//        policy => policy
+//            .WithOrigins("http://localhost:5173") // React dev server
+//            .AllowAnyMethod()
+//            .AllowAnyHeader());
+//});
+
+//app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
